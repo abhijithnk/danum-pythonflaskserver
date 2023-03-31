acac77a7d59b25ac2c66245960c0142bacd3d148
abhijithnk <kashy015@umn.edu>
2023-03-30 22:10:57 -0500

    Made changes in app.py to see how the pipeline is triggered

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask
 
 app = Flask("My Flask Application")
@@ -8,6 +9,16 @@ def hello():
     return "<h1>Hello World!</h1>"
 
 
+if __name__=="__main__":
+    app.run(debug=True) 
+    # When no port is specified, starts at default port 5000
+"""
+
+from flask import Flask
+app = Flask("My Flask Application")
+@app.route("/")
+def hello():
+    return "<h1>My First Hello World!</h1>"
 if __name__=="__main__":
     app.run(debug=True) 
     # When no port is specified, starts at default port 5000
